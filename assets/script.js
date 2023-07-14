60987fa201eeb8d8facd8c45a33e0f752ee76e41
Garrett <garrettswink421@gmail.com>
2023-07-13 20:57:21 -0400

    Final Notations Ahead of Coding + JQuery Added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
-
-
-
+// Garrett's Variables and Functions
 const test = document.querySelector('.headlines');
 
 test.innerHTML = 'hello, world!';
+
+
+// Jasmin's Variables and Functions
+
+
+// Kyle's Variable and Functions
